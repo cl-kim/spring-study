a27df14898b42eed3873559811be7f738c5a07de
cl-kim <nias0327@naver.com>
2023-10-30 17:17:44 +0900

    feat: Implement RateDiscountPolicy

---
+++ spring-basic/src/main/java/hello/springbasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.springbasic.discount;
+
+import hello.springbasic.member.Grade;
+import hello.springbasic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private final int DISCOUNT_PERCENT = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * DISCOUNT_PERCENT / 100;
+        } else {
+            return 0;
+        }
+    }
+}
