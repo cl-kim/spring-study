536ae794c27ed582d142f3f7bfa8bd7f5fb0582b
cl-kim <nis0327@naver.com>
2023-11-04 20:24:40 +0900

    feat: Implement @Qualifier to resolve NoUniqueBeanDefinitionException

--- spring-basic/src/main/java/hello/springbasic/discount/RateDiscountPolicy.java
+++ spring-basic/src/main/java/hello/springbasic/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.springbasic.discount;
 
 import hello.springbasic.member.Grade;
 import hello.springbasic.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("mainDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private final int DISCOUNT_PERCENT = 10;
--- spring-basic/src/main/java/hello/springbasic/order/OrderServiceImpl.java
+++ spring-basic/src/main/java/hello/springbasic/order/OrderServiceImpl.java
@@ -3,15 +3,21 @@ package hello.springbasic.order;
 import hello.springbasic.discount.DiscountPolicy;
 import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
