dfd6ed016f3a76e1ab0e6ea0e3fc619911328297
cl-kim <nis0327@naver.com>
2023-11-05 19:49:18 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 javax.inject.Provider 사용

--- spring-basic/src/test/java/hello/springbasic/scope/SingletonWithPrototypeTest1.java
+++ spring-basic/src/test/java/hello/springbasic/scope/SingletonWithPrototypeTest1.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,10 +44,10 @@ public class SingletonWithPrototypeTest1 {
     static class ClientBean {
 
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeans;
+        private Provider<PrototypeBean> provider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeans.getObject();
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
