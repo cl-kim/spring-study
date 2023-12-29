4d3a2826305f1c253b9a91cc1c9c784b3d495ffd
cl-kim <nias0327@naver.com>
2023-12-29 18:40:42 +0900

    @ControllerAdvice

--- exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-public class ApiExceptionV2Controller {
+public class ApiExceptionV3Controller {
 
-    @GetMapping("/api/v2/members/{id}")
+    @GetMapping("/api/v3/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
