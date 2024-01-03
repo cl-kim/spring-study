8e057dbcfbfcb03da7289ee332ad888841e19877
cl-kim <nias0327@naver.com>
2024-01-03 14:27:04 +0900

    JDBC 개발 - 수정, 삭제

--- spring-db/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ spring-db/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -45,7 +45,7 @@ public class MemberRepositoryV0 {
             pstmt.setString(1, memberId);
 
             rs = pstmt.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 Member member = new Member();
                 member.setMemberId(rs.getString("member_id"));
                 member.setMoney(rs.getInt("money"));
@@ -53,11 +53,52 @@ public class MemberRepositoryV0 {
             } else {
                 throw new NoSuchElementException("member not found memberId =" + memberId);
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             log.error("db error", e);
             throw e;
         } finally {
             close(con, pstmt, rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money = ? where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
         }
     }
 
