ce28f543ffc04cc6c590eb5896999435524c59a9
cl-kim <nias0327@naver.com>
2024-02-07 17:21:25 +0900

    스프링 데이터 JPA 적용2

--- spring-db/itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
+++ spring-db/itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -1,13 +1,13 @@
 package hello.itemservice.repository.jpa;
 
 import hello.itemservice.domain.Item;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface SpringDataJapItemRepository extends JpaRepository<Item, Long> {
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByItemNameLike(String itemName);
 
